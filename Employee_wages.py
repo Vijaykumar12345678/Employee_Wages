4e24d850b678c84b5a4e0e073074a24d2fb10452
vijay1 <vijaykumarynh7@gmail.com>
2024-08-14 13:51:23 +0530

    [Vijay] add: To check the employee is present or not.

--- Employee_wages.py
+++ Employee_wages.py
@@ -4,10 +4,27 @@
 @Date: 2024-08-13
 @Last Modified by: Vijay Kumar M N
 @Last Modified: 2024-08-13
-@Title : python program to display the Employee wages welcome.
+@Title : python program to check employee is present or not.
 
 '''
+import random
+def check_employee(employee):
+    """
+    Description:
+    This funtion is to check that employee is present or absent.
+    parameters:
+    employee : int which is to check if it is 1 then present  or else absent.
+    return:None """
+    if employee==1:
+        print("The Employee is Present.")
+    else:
+        print("The Employee is Absent")
 def main():
-    print("""Welcome to Employee Wage Computation""")
+    number=random.randint(0,1)
+    check_employee(number)
+
 if __name__=="__main__":
     main()
+
+
+
