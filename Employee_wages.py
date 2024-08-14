5c9ec7b2a826d9d3f07f76152aeb90c054292dc8
vijay1 <vijaykumarynh7@gmail.com>
2024-08-14 14:06:27 +0530

    [Vijay] add:  python program to check employee present full time or part and then calculate the wages.

--- Employee_wages.py
+++ Employee_wages.py
@@ -19,18 +19,18 @@ def check_employee(employee):
     wage_per_hour=20
     full_time=8
     part_time=4
-    
-    if employee==1:
-        check_time=random.randint(0,1)
-        if check_time==0:
-            daily_wage=wage_per_hour*part_time
-            print(f"The Employee is part time Present and employee wage is {daily_wage} ")
-        else:
-            print(f"The Employee is full time Present and employee wage is {wage_per_hour*full_time}")
-
-
-    else:
-        print("The Employee is Absent and Employee wage is 0")
+    print("""Welcome to Employee Wage Computation""")
+    match employee:
+        case 1:
+            check_time=random.randint(0,1)
+            match check_time:
+                case 0:
+                    daily_wage=wage_per_hour*part_time
+                    print(f"The Employee is part time Present and employee wage is {daily_wage} ")
+                case _:
+                    print(f"The Employee is full time Present and employee wage is {wage_per_hour*full_time}")
+        case _:
+            print("The Employee is Absent and Employee wage is 0")
 def main():
     employee=random.randint(0,1)
     check_employee(employee)
