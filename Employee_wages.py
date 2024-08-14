116a3953325a569bfbf4c4a5f34983d0a2327dd7
vijay1 <vijaykumarynh7@gmail.com>
2024-08-14 13:49:51 +0530

    [Vijay] add: To display the Welcome message.

---
+++ Employee_wages.py
@@ -0,0 +1,13 @@
+'''
+
+@Author:Vijay Kumar M N
+@Date: 2024-08-13
+@Last Modified by: Vijay Kumar M N
+@Last Modified: 2024-08-13
+@Title : python program to display the Employee wages welcome.
+
+'''
+def main():
+    print("""Welcome to Employee Wage Computation""")
+if __name__=="__main__":
+    main()
