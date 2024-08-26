0202eabdb7514dda6647040336cf1d29f3930872
vijay1 <vijaykumarynh7@gmail.com>
2024-08-26 16:44:52 +0530

    [Vijay] add:python program to check employee present full time or part and then calculate the wages.

--- Employee_wages.py
+++ Employee_wages.py
@@ -29,14 +29,13 @@ def check_employee():
         check_time=random.randint(0,2)
         if check_time==0:
             return "Present"
-        elif check_time==1:
-            return "Parttime"
         else:
-            return "Absent"
+            return "Parttime"
+        
     else:
         return "Absent"
 
-def calculate_wages(wage_per_hour,check_time):
+def calculate_wages(check_time):
     """
     Description:
     This function is used to calculate the wages of the Employee
@@ -47,7 +46,7 @@ def calculate_wages(wage_per_hour,check_time):
     
     return:
     int:calculated the wages   """
-    return wage_per_hour*check_time       
+    return  WAGE_PER_HOUR *check_time       
 
 def main():
     print("""-------Welcome to Employee Wage Computation-------""")
@@ -58,13 +57,13 @@ def main():
     #employee is full time present
 
     if employee=="Present":
-        daily_wage=calculate_wages( WAGE_PER_HOUR,FULL_TIME)
-        print(f"The Employee is Present and employee wage is {daily_wage}")
+        daily_wage=calculate_wages( FULL_TIME)
+        print(f"The Employee is Present fulltime and employee wage is {daily_wage}")
     #employee is part time present
     
     elif employee=="Parttime":
-        daily_wage=calculate_wages( WAGE_PER_HOUR,PART_TIME)
-        print(f"The Employee is Present and employee wage is {daily_wage}")
+        daily_wage=calculate_wages( PART_TIME)
+        print(f"The Employee is Present parttime and employee wage is {daily_wage}")
     
     else:
         print(f"The Employee is Absent and employee wage is {0}")
