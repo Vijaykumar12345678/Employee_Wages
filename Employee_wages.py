c110fd5c0c1586369d0d04e944b62c72e9051574
vijay1 <vijaykumarynh7@gmail.com>
2024-08-26 13:49:30 +0530

    [Vijay] add: python program to check employee present full time or part and then calculate the wages using switch

--- Employee_wages.py
+++ Employee_wages.py
@@ -8,35 +8,53 @@
 
 '''
 import random
-def check_employee(employee):
+
+WAGE_PER_HOUR = 20
+FULL_TIME = 8
+PART_TIME= 4
+
+
+def check_employee():
     """
     Description:
-    This funtion is to check that employee is present or absent and calculate the wages for the full time employee and part time employee.
-    parameters:
-    employee : int which is to check if it is 1 then present  or else absent.
-    return:None """
+    This function checks if the employee is present or absent and calculates the wages for full-time or part-time employees.
     
-    wage_per_hour=20
-    full_time=8
-    part_time=4
-    print("""Welcome to Employee Wage Computation""")
+    Parameters:
+    None
+    
+    Returns:
+    None
+    """
+    
+    employee = random.randint(0, 1)
     match employee:
         case 1:
-            check_time=random.randint(0,1)
+            check_time = random.randint(0, 1)
             match check_time:
                 case 0:
-                    daily_wage=wage_per_hour*part_time
-                    print(f"The Employee is part time Present and employee wage is {daily_wage} ")
-                case _:
-                    print(f"The Employee is full time Present and employee wage is {wage_per_hour*full_time}")
-        case _:
+                    daily_wage = calculate_wage(PART_TIME)
+                    print(f"The Employee is part-time Present and employee wage is {daily_wage}")
+                case 1:
+                    daily_wage = calculate_wage(FULL_TIME)
+                    print(f"The Employee is full-time Present and employee wage is {daily_wage}")
+        case 0:
             print("The Employee is Absent and Employee wage is 0")
+def calculate_wage(hours):
+    """
+    Description:
+    This function calculates the wage based on the number of hours worked.
+    
+    Parameters:
+    hours : int The number of hours the employee worked.
+    
+    Returns:
+    int: The calculated wage.
+    """
+    return WAGE_PER_HOUR * hours
+
 def main():
-    employee=random.randint(0,1)
-    check_employee(employee)
+    print("--------Welcome to Employee Wage Computation-------")    
+    check_employee()
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
-
-
-
